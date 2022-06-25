b666a714b1671d60c19134cfb8e58eb15d475ff9
Salsabil El Gharably <78717168+salsabilelgharably@users.noreply.github.com>
2022-06-25 21:51:05 +0200

    Time: 26 ms (98.15%), Space: 13.9 MB (10.05%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -7,8 +7,8 @@ class Solution:
                 Goal += "G"
             elif command[i] == "(" and command[i+1] == ")":
                 Goal += "o"
-                i = i+2
+
             elif command[i] == "(" and command[i+1] == "a":
                 Goal += "al"
-                i = i+4
+                i = i + 4
         return Goal
