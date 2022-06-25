0398f9cb52000ad8c455e44bf55551975c4497ce
Salsabil El Gharably <78717168+salsabilelgharably@users.noreply.github.com>
2022-06-25 21:48:03 +0200

    Time: 51 ms (33.82%), Space: 14 MB (10.05%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,16 +1,12 @@
 class Solution:
     def interpret(self, command: str) -> str:
         Goal =""
-        cmd =""
+
         for i in range(len(command)):
             if command[i] == "G":
                 Goal += "G"
-            elif command[i] == "(" or ")" or "a" or "l":
-                cmd += command[i]
-            if cmd == "()":
+            elif command[i] == "(" and command[i+1] == ")":
                 Goal += "o"
-                cmd = ""
-            elif cmd == "(al)":
+            elif command[i] == "(" and command[i+1] == "a":
                 Goal += "al"
-                cmd =""
         return Goal
