a298c36f552f785ece34324929584896fb7acf17
Salsabil El Gharably <78717168+salsabilelgharably@users.noreply.github.com>
2022-08-03 19:13:44 +0200

    Time: 52 ms (49.81%), Space: 13.8 MB (82.39%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -6,7 +6,7 @@ class Solution:
             return True
         for i in range(len(t)):
             if s[0] == t[i]:
-                s = s[:0] + s[(0+1):]
+                s = s[1:]
                 if s =='':
                     return True
         return False
