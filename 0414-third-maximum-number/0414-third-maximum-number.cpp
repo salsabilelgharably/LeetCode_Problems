cfcc0d8c0223f499d03235c36858cd0ec29482c5
Salsabil El Gharably <78717168+salsabilelgharably@users.noreply.github.com>
2023-02-12 07:17:36 +0200

    Time: 6 ms (75.18%), Space: 9 MB (99.43%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,25 @@
+bool com(int a,int b)
+{
+    return a>b;
+}
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), com);
+        int prev = nums[0];
+        int c = 1;
+        for(int i =1; i < nums.size(); i++){
+            if(nums[i] == prev){
+                continue;
+            }
+            else{
+                c++;
+                prev = nums[i];
+            }
+            if(c==3){
+                return prev;
+            }   
+        }
+        return nums[0];
+    }
+};
