d29d4d307af0d5784777cd7e5610fd99105bf9b7
Salsabil El Gharably <78717168+salsabilelgharably@users.noreply.github.com>
2023-02-17 22:03:08 +0200

    Time: 0 ms (100.00%), Space: 6 MB (57.19%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,19 +4,27 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long st=1;
-        long et=n;
-        long mid=(st+et)/2;
-        while(st<=et)
-        {
-            mid=(st+et)/2;
-            if(isBadVersion(mid)==true && isBadVersion(mid-1)==false)
+        long left =1;
+        long right =n;
+        long mid=(left+right)/2;
+        while(left<=right){
+            mid=(right+left)/2;
+            if(isBadVersion(mid) && !isBadVersion(mid-1)){
                 return mid;
-            if(isBadVersion(mid)==true)
-                et=mid-1;
-            else
-                st=mid+1;
+            }
+            else if(!isBadVersion(mid) && isBadVersion(mid+1)){
+                return mid+1;
+            }
+            else if(isBadVersion(mid)){
+                right =mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+            
+
         }
-        return mid;    
+        return mid;
     }
 };
+
